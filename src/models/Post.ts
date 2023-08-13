6a13c6d25403932296d45180183cc42fd85a4b4d
Robert Krause <robkrause91@gmail.com>
2023-08-13 18:16:06 +0200

    feat: init project, add basic blog structure and style

---
+++ src/models/Post.ts
@@ -0,0 +1,18 @@
+export interface Post {
+	date: string
+	uri: string
+	title: string
+
+	// featuredImage {
+	//   node {
+	//     srcSet
+	//     sourceUrl
+	//     altText
+	//     mediaDetails {
+	//       height
+	//       width
+	//     }
+	//   }
+	// }
+	//   }
+}
